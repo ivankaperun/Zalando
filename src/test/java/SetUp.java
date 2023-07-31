036d69a61fae76e31155dfe79f3761ff197356eb
ivankaperun <ivkaloz@gmail.com>
2023-07-31 10:45:00 +0300

    added parameters usage to .xml file

--- src/test/java/SetUp.java
+++ src/test/java/SetUp.java
@@ -3,21 +3,24 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Parameters;
 
 import java.time.Duration;
 
-import static utils.Constants.*;
 
 public class SetUp {
     public WebDriver driver;
 
     @BeforeClass
-    protected void setUpBrowser() {
-        WebDriverManager.chromedriver().setup();
-        driver = new ChromeDriver();
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(DEFAULT_TIMEOUT));
-        driver.get(MAIN_URL);
-        driver.manage().window().maximize();
+    @Parameters({"browser","url", "timeout"})
+    protected void setUpBrowser(String browser, String url, long timeout) {
+        if(browser.equalsIgnoreCase("chrome")) {
+            WebDriverManager.chromedriver().setup();
+            driver = new ChromeDriver();
+            driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(timeout));
+            driver.get(url);
+            driver.manage().window().maximize();
+        }
     }
 
     @AfterClass
