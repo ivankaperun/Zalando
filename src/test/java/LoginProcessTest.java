c7171f053c08edacd8e27e7413bbbc63657a5dbd
ivankaperun <ivkaloz@gmail.com>
2023-08-28 17:52:57 +0300

    refactoring

--- src/test/java/LoginProcessTest.java
+++ src/test/java/LoginProcessTest.java
@@ -2,7 +2,6 @@ import org.testng.annotations.Test;
 import pages.IndexPage;
 import pages.LoginPage;
 
-import static org.testng.Assert.assertEquals;
 
 public class LoginProcessTest extends SetUp {
 
@@ -10,7 +9,6 @@ public class LoginProcessTest extends SetUp {
     public void loginProcessTest () throws InterruptedException {
         IndexPage indexPage = new IndexPage(driver);
         LoginPage loginPage = new LoginPage(driver);
-        String expected_result = "Your account";
 
         indexPage.hoverUserAccountButton();
         indexPage.clickOnLoginButton();
